0493d76eb92a6d40a3cf70be635b305625cc6e8f
oztugrul <45922919+oztugrul@users.noreply.github.com>
2019-07-31 09:49:02 +0300

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -71,7 +71,7 @@ names(extracted_data)<-gsub("angle","Angle",names(extracted_data))
 names(extracted_data)<-gsub("\\.\\.\\.","\\.",names(extracted_data))
 names(extracted_data)<-gsub("\\.\\.","\\.",names(extracted_data))
 
-
+names(extracted_data)
 #5.From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
 #Using dplyr package
